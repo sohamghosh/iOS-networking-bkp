e419dc9b5a366b3df0f46eb2ab39ce4f9966c7b0
Soham Ghosh <sohamghosh@gmail.com>
2013-12-04 02:18:48 +0530

    With AFNetworking pod.

---
+++ Networking/PhotoViewController.m
@@ -0,0 +1,34 @@
+#import "PhotoViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface PhotoViewController ()
+
+@end
+
+@implementation PhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    UIImageView *imageView = [[UIImageView alloc] init];
+    imageView.frame = CGRectMake(10, 10, 300, 300);
+    [imageView setImageWithURL:[NSURL URLWithString:self.imageFileName]];
+    [self.view addSubview:imageView];
+    
+    UILabel *imageTitleLabel = [[UILabel alloc] init];
+    imageTitleLabel.text = self.imageTitle;
+    imageTitleLabel.frame = CGRectMake(11,320,300,40);
+    [self.view addSubview:imageTitleLabel];
+}
+
+@end
